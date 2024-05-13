a5fd4b70cbaefe5c4aa445d311bc524ea58623bf
kali <kali@kali.kali>
2024-05-13 13:45:25 +0530

    testing order

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -10,48 +10,50 @@ const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::order.order',({ strapi }) => ({
     async create(ctx) {
-      const { products,subTotal,order_id} = ctx.request.body;
+
+      console.log(ctx.request);
+      // const { products,subTotal,order_id} = ctx.request.body;
   
       
      
   
-      try {
-        if(order_id===0){
+      // try {
+      //   if(order_id===0){
         
        
-        const razorpay = new Razorpay({
-          key_id: process.env.RAZORPAY_KEY_ID,
-          key_secret: process.env.RAZORPAY_SECRET,
-        });
+      //   const razorpay = new Razorpay({
+      //     key_id: process.env.RAZORPAY_KEY_ID,
+      //     key_secret: process.env.RAZORPAY_SECRET,
+      //   });
   
   
-        const options = {
-          amount:subTotal*100,
-          currency: "INR",
-          receipt: "razor@mail.com",
-        };
+      //   const options = {
+      //     amount:subTotal*100,
+      //     currency: "INR",
+      //     receipt: "razor@mail.com",
+      //   };
   
-        const order = await razorpay.orders.create(options);
+      //   const order = await razorpay.orders.create(options);
   
-        if (!order) {
-          ctx.response.status = 500;
-        }
+      //   if (!order) {
+      //     ctx.response.status = 500;
+      //   }
   
-        ctx.send(order);
-      }
-      else{
-        console.log(order_id);
-        await strapi
-          .service("api::order.order")
-          .create({ data: { products, orderId:order_id } });
-          ctx.response.status=200;
-      }
+      //   ctx.send(order);
+      // }
+      // else{
+      //   console.log(order_id);
+      //   await strapi
+      //     .service("api::order.order")
+      //     .create({ data: { products, orderId:order_id } });
+      //     ctx.response.status=200;
+      // }
      
-      } catch (error) {
-        ctx.response.status = 500;
-        console.log(error);
-        return { error };
-      }
+      // } catch (error) {
+      //   ctx.response.status = 500;
+      //   console.log(error);
+      //   return { error };
+      // }
     },
   }));
   
