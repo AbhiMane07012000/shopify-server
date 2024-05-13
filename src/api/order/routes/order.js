6af08885cc9e78d3022df17523435ddfb7dca1a7
kali <kali@kali.kali>
2024-05-13 15:53:22 +0530

    order controller updated.

--- src/api/order/routes/order.js
+++ src/api/order/routes/order.js
@@ -1,56 +1,47 @@
-import Razorpay from 'razorpay';
-'use strict';
+"use strict";
 
 /**
  * order router
  */
+import Razorpay from "razorpay";
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
+const { createCoreRouter } = require("@strapi/strapi").factories;
 
-module.exports = createCoreRouter('api::order.order', ({ strapi }) => ({
-    async create(ctx) {
-      const { products,subTotal,order_id} = ctx.request.body;
-  
-      
-     
-  
-      try {
-        if(order_id===0){
-        
-       
+module.exports = createCoreRouter("api::order.order", ({ strapi }) => ({
+  async create(ctx) {
+    const { products, subTotal, order_id } = ctx.request.body;
+
+    try {
+      if (order_id === 0) {
         const razorpay = new Razorpay({
           key_id: process.env.RAZORPAY_KEY_ID,
           key_secret: process.env.RAZORPAY_SECRET,
         });
-  
-  
+
         const options = {
-          amount:subTotal*100,
+          amount: subTotal * 100,
           currency: "INR",
           receipt: "razor@mail.com",
         };
-  
+
         const order = await razorpay.orders.create(options);
-  
+
         if (!order) {
           ctx.response.status = 500;
         }
-  
+
         ctx.send(order);
-      }
-      else{
+      } else {
         console.log(order_id);
         await strapi
           .service("api::order.order")
-          .create({ data: { products, orderId:order_id } });
-          ctx.response.status=200;
+          .create({ data: { products, orderId: order_id } });
+        ctx.response.status = 200;
       }
-     
-      } catch (error) {
-        ctx.response.status = 500;
-        console.log(error);
-        return { error };
-      }
-    },
-  }));
-  
+    } catch (error) {
+      ctx.response.status = 500;
+      console.log(error);
+      return { error };
+    }
+  },
+}));
