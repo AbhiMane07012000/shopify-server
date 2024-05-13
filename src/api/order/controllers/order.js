d82beef924c9ef62c39db17597d4fae8c9aa7cae
kali <kali@kali.kali>
2024-05-13 13:43:48 +0530

    order table created.

---
+++ src/api/order/controllers/order.js
@@ -0,0 +1,57 @@
+'use strict';
+const Razorpay = require("razorpay");
+
+
+/**
+ * order controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::order.order',({ strapi }) => ({
+    async create(ctx) {
+      const { products,subTotal,order_id} = ctx.request.body;
+  
+      
+     
+  
+      try {
+        if(order_id===0){
+        
+       
+        const razorpay = new Razorpay({
+          key_id: process.env.RAZORPAY_KEY_ID,
+          key_secret: process.env.RAZORPAY_SECRET,
+        });
+  
+  
+        const options = {
+          amount:subTotal*100,
+          currency: "INR",
+          receipt: "razor@mail.com",
+        };
+  
+        const order = await razorpay.orders.create(options);
+  
+        if (!order) {
+          ctx.response.status = 500;
+        }
+  
+        ctx.send(order);
+      }
+      else{
+        console.log(order_id);
+        await strapi
+          .service("api::order.order")
+          .create({ data: { products, orderId:order_id } });
+          ctx.response.status=200;
+      }
+     
+      } catch (error) {
+        ctx.response.status = 500;
+        console.log(error);
+        return { error };
+      }
+    },
+  }));
+  
